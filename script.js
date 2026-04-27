61e4aa5c3e1fc9f64161fafcc901286664f45449
Ali Ahmed Abdul Hussein Zarki <2501230089@iku.edu.iq>
2026-04-28 01:34:04 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const btn = document.querySelector(".primary");
+  btn.addEventListener("click", () => {
+    console.log("APK download started");
+  });
+});
